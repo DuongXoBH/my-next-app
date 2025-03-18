84c2cdeaa484beec5dbed93cb7d91ece551920f2
unknown <xonghean@gmail.com>
2025-03-18 12:29:14 +0700

    fix test component deloy

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,30 +1,3 @@
-"use client";
-
-import { useState } from "react";
-import { CldUploadWidget } from "next-cloudinary";
-
-export default function UploadImage() {
-  const [imageUrl, setImageUrl] = useState<string>("");
-  console.log("🚀 ~ UploadImage ~ imageUrl:", imageUrl)
-
-  return (
-    <div className="flex flex-col items-center">
-      <CldUploadWidget
-        uploadPreset={process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET}
-        onSuccess={(result) => {
-          if (result.event === "success") {
-            setImageUrl((result.info as { secure_url: string }).secure_url);
-
-          }
-        }}
-      >
-        {({ open }) => (
-          <button onClick={() => open()} className="p-2 bg-blue-500 text-white rounded">
-            Upload Image
-          </button>
-        )}
-      </CldUploadWidget>
-
-    </div>
-  );
+export default function Test(){
+  return <h1>AA</h1>
 }
