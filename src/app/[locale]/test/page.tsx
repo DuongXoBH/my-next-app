3985770062f70e87a605208562fbda6e3ff1ab70
unknown <xonghean@gmail.com>
2025-04-04 13:48:14 +0700

    redeploy send invoice dialog

--- src/app/[locale]/test/page.tsx
+++ src/app/[locale]/test/page.tsx
@@ -3,6 +3,7 @@ export default function Test() {
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center">
       <h1 className="text-2xl font-bold">Test Page</h1> 
+      <h2>test</h2>
     </div>
   )
 }
